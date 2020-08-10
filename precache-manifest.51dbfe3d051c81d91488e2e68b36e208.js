7907d7bf8ae50d223450e5010f327c9303ef3df8
Michael <mjfl780@gmail.com>
2020-08-10 00:36:01 -0400

    Updates

---
+++ precache-manifest.51dbfe3d051c81d91488e2e68b36e208.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61e73d98ce2d55a95279a1decf50d23e",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "a733e1b295c9af6b9796",
+    "url": "/react-tutorial/static/css/main.8a18c7c4.chunk.css"
+  },
+  {
+    "revision": "9d2ae3c2628a4c5fd0d4",
+    "url": "/react-tutorial/static/js/2.67691ccd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.67691ccd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a733e1b295c9af6b9796",
+    "url": "/react-tutorial/static/js/main.293daea8.chunk.js"
+  },
+  {
+    "revision": "d2980c81d16204d1adff",
+    "url": "/react-tutorial/static/js/runtime-main.fba2e450.js"
+  }
+]);
